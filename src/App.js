98cbcb9840c9fbb907c38fa7854f020808e2e083
shreya-mishra <shreyamishra040398@gmail.com>
2023-08-29 18:14:33 +0530

    solve the problem and wrote the test cases

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { useState } from 'react';
 import './App.css';
 
 const App = () => {
-  return(
-    <div>
-    <button title="Click me!" onClick={() => console.log('clicked me ')} />
+  const [flag, setFlag] = useState(false);
+  const timeStamp = new Date().toLocaleString()
+  const handleClick = () => {
+    setFlag((prev) => !prev)
+  }
+  return (
+    <div className='app'>
+      {!flag ? (<button className="fade-button" onClick={handleClick}>Click me!</button>) : (<div className='time'>
+        {timeStamp}
+      </div>)}
+
+
     </div>
   )
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,38 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, fireEvent, screen, toHaveStyle } from '@testing-library/react';
 import App from './App';
 
-test('renders Button', () => {
+test('renders button initially', () => {
   render(<App />);
+  const buttonElement = screen.getByText('Click me!');
+  expect(buttonElement).toBeInTheDocument();
+  expect(buttonElement).toHaveClass('fade-button');
 });
+
+test('displays timestamp after button click', async () => {
+  render(<App />);
+  const buttonElement = screen.getByText('Click me!');
+  
+  fireEvent.click(buttonElement); // Simulate button click
+  
+  const timestampElement = await screen.findByText(new RegExp('.+')); // Any text content
+  
+  expect(buttonElement).not.toBeInTheDocument(); // Button should disappear
+  expect(timestampElement).toBeInTheDocument(); // Timestamp should appear
+  expect(timestampElement).toHaveClass('time');
+});
+
+test('timestamp format is correct', async () => {
+  render(<App />);
+  const buttonElement = screen.getByText('Click me!');
+  
+  fireEvent.click(buttonElement); // Simulate button click
+  
+  const timestampElement = await screen.findByText(new RegExp('.+')); // Any text content
+  const timestampText = timestampElement.textContent;
+
+  // Match the timestamp using a regular expression that covers common formats
+  const timestampRegex = /^\d{1,2}\/\d{1,2}\/\d{4}, \d{1,2}:\d{2}(:\d{2})? [APap][Mm]$/;
+  expect(timestampText).toMatch(timestampRegex);
+});
+
